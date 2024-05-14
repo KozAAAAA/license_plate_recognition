6e26ba9102b64b293b95ee65c5c0f06100c035a1
KozAAAAA <mateusz-koza@outlook.com>
2024-05-14 15:13:07 +0200

    Add code that skipps 10 images

--- processing/utils.py
+++ processing/utils.py
@@ -11,13 +11,13 @@ def _sort_corners(corners: np.ndarray) -> np.ndarray:
     corners = sorted(corners, key=lambda x: x[0][0])
     left_top, left_bottom = sorted(corners[:2], key=lambda x: x[0][1])
     right_top, right_bottom = sorted(corners[2:], key=lambda x: x[0][1])
-    corners = np.array([left_bottom, left_top, right_top, right_bottom])
+    corners = np.array([left_bottom[0], left_top[0], right_top[0], right_bottom[0]])
     return corners
 
 def get_license_plate(image: np.ndarray) -> np.ndarray:
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    bilateral = cv2.bilateralFilter(grey, 5, 50, 50)
-    canny = cv2.Canny(bilateral, 20, 60)
+    bilateral = cv2.bilateralFilter(grey, 11, 11, 11)
+    canny = cv2.Canny(bilateral, 30, 90)
     contours = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
@@ -25,34 +25,35 @@ def get_license_plate(image: np.ndarray) -> np.ndarray:
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         width_plate_to_image_ratio = w / image.shape[1]
-        if width_plate_to_image_ratio > MIN_WIDTH_PLATE_TO_IMAGE_RATIO:
-            peri = cv2.arcLength(contour, True)
-            corners = cv2.approxPolyDP(contour, 0.02 * peri, True)
-            if len(corners) != 4:
-                continue
-
+        peri = cv2.arcLength(contour, True)
+        corners = cv2.approxPolyDP(contour, 0.02 * peri, True)
+        if width_plate_to_image_ratio > MIN_WIDTH_PLATE_TO_IMAGE_RATIO and len(corners) == 4:
+            
             corners = _sort_corners(corners)
-            cv2.circle(image, tuple(corners[0][0]), 5, (0, 255, 0), -1)
-            cv2.circle(image, tuple(corners[1][0]), 5, (0, 0, 255), -1)
-            cv2.circle(image, tuple(corners[2][0]), 5, (255, 0, 0), -1)
-            cv2.circle(image, tuple(corners[3][0]), 5, (255, 255, 0), -1)
+            left_top, left_bottom, right_top, right_bottom = corners
+            cv2.circle(image, left_top, 5, (0, 255, 0), -1)
+            cv2.circle(image, left_bottom, 5, (0, 0, 255), -1)
+            cv2.circle(image, right_top, 5, (255, 0, 0), -1)
+            cv2.circle(image, right_bottom, 5, (255, 255, 0), -1)
 
             # accept only with proper aspect ratio (add to list and later decide)
             license_plate = image[y : y + h, x : x + w]
             return license_plate
-
+    
     return None
+
 
 
 def perform_processing(image: np.ndarray) -> str:
     image = cv2.resize(image, IMG_SIZE)
-    # cropped = image[200:600, :] # Kinda cheating lol
-    license_plate = get_license_plate(image)
-
-    if license_plate is None:
-        return "NOLICENSE"
+    cropped = image[100:700, :] # Kinda cheating lol
+    license_plate = get_license_plate(cropped)
+    if license_plate is None:       
+        perform_processing.skipped += 1
+        print(f"Skipped {perform_processing.skipped} images")
+        return "No license plate found"
 
     cv2.imshow("image", license_plate)
     cv2.waitKey(0)
-
     return "PO12345"
+perform_processing.skipped = 0
